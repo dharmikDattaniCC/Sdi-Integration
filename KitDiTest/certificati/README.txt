2ed22a1fa0fd7eb011768c30bff31d57baf93ec2
hiteshD01 <hitesh.crestcoder@gmail.com>
2025-01-21 12:14:52 +0530

    certificates added

---
+++ KitDiTest/certificati/README.txt
@@ -0,0 +1,17 @@
+CERTIFICATI DI TEST
+- testservizi.fatturapa.it.cer: Certificato SERVER esposto dai servizi di test del Sistema di Interscambio
+
+- SistemaInterscambioFatturaPATest.cer: Parte pubblica del certificato CLIENT utilizzato dal Sistema di Interscambio per invocare i servizi di test da voi esposti
+
+
+CERTIFICATI DI PRODUZIONE
+- servizi.fatturapa.it.cer: Certificato SERVER esposto dai servizi del Sistema di Interscambio
+
+- SistemaInterscambioFatturaPA.cer: Parte pubblica del certificato CLIENT utilizzato dal Sistema di Interscambio per invocare i servizi da voi esposti
+
+
+CERTIFICATI DI CA
+- caentrate.cer: certificato di CA per ambiente di produzione
+- CAEntratetest.cer: certificato di CA per validare il certificato SdI di test
+- Sectigo RSA.cer: certificato di Ca per servizi.fatturapa.it.cer per Ambiente di produzione 
+- UserTrustCA.cer: certificato di CA per servizi.fatturapa.it.cer per ambiente di produzione 
--- src/index.ts
+++ src/index.ts
@@ -9,44 +9,14 @@ import './soapServer'; // Import SOAP server logic
 
 const PORT: number = 5009;
 const server: http.Server = http.createServer(app);
-const access: mysql.ConnectionOptions = {
-  host: 'cp-13.webhostbox.net',
-  user: 'projej55_pobou',
-  password: '_y)mlV!}]Lg9',
-  database: 'projej55_pobo',
-}
 
 async function startServer() {
   // Execute SQL script to create tables
   // const scriptPath = path.join(__dirname, '../sql/onboarding-schema-creatre.sql');
   // await runSqlScript(scriptPath, access);
 
-  // Sequelize database setup
-  await sequelize
-  .sync()
-  .then(() => logger.info("Database synchronized"))
-  .catch((error) => logger.error("Error syncing database:", error));
-
-  // DB connection
-  const conn: mysql.Connection = mysql.createConnection(access);
-  
-  conn.connect((err: mysql.QueryError | null) => {
-    if (err) {
-      if (Array.isArray(err)) {
-        err.forEach(err => logger.error(`Individual error: ${err}`))
-      } else {
-        console.log(err.stack);
-        
-        logger.error(`🐞🐞 Error connecting to database: ${err}`);
-      }
-      process.exit(1);
-    }
-
-    logger.info("🚀🚀 Database connection created");
-    
-    server.listen(PORT, () => {
-      logger.info(`Server is running on ${PORT}...`);
-    });
+  server.listen(PORT, () => {
+    logger.info(`Server is running on ${PORT}...`);
   });
 }
 
